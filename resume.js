e3afb1dbb91f496bbd5b2a377109269d1b1c06ba
Christian Rudder <crubber@bu.edu>
2024-09-19 11:00:14 -0400

    mobile responsivness

--- resume.js
+++ resume.js
@@ -29,5 +29,88 @@ function checkAge() {
     else {
         alert.innerHTML = "You may not view this content."
     }
+}
+
+function operation (op) {
+    let a = document.getElementById("f-number");
+    let b = document.getElementById("s-number");
+    let c = document.getElementById("o-number");
+    c.style.color = "black";
+    op(a,b,c);
+}
+
+function b_add(x,y,c) {
+    let a = x.value;
+    let b = y.value;
     
+
+    c.innerHTML = Number(a) + Number(b);
+}
+function b_sub(x,y,c) {
+    let a = x.value;
+    let b = y.value;
+
+    let t = Number(a) - Number(b);
+
+    if(t<0) {
+        c.style.color = "red";
+    }
+    
+    c.innerHTML = t;
+}
+function b_mul(x,y,c) {
+    let a = x.value;
+    let b = y.value;
+    
+
+    c.innerHTML = Number(a) * Number(b);
+}
+function b_div(x,y,c) {
+    let a = x.value;
+    let b = y.value;
+    
+
+    c.innerHTML = Number(a) / Number(b);
+}
+function b_pow(x,y,c) {
+
+    let a = x.value;
+    let b = y.value;
+    
+    if(b == 0) { //weak equality on purpose
+   
+        c.innerHTML = 1;
+        return;
+    }
+
+    let d = a;
+
+    for(let i = 0; i < b-1; i++) {
+        a =  d * a;
+        console.log(a)
+    }
+    
+
+    c.innerHTML = a;
+}
+function b_c(x,y,c) {
+   
+    
+
+    x.value = null;
+    y.value = null;
+    c.innerHTML = "_";
+}
+
+function accordClose() {
+    console.log("hello")
+    document.getElementById("mob-nav").style.display = "none";
+    document.getElementById("mob-nav-open").style.display = "flex";
+
+}
+function accordOpen() {
+    console.log("hello")
+    document.getElementById("mob-nav").style.display = "flex";
+    
+    document.getElementById("mob-nav-open").style.display = "none";
 }
